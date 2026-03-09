ff3172b7dd4fdef4022865e8f3e4a986c0b01e8d
k570b468 <55644723+k570b468@users.noreply.github.com>
2026-03-09 11:58:43 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
 const gameText=document.getElementByld('game-text2');
 const userInput=document.getElementByld('user-input2');
 const submitBtn =document.getElementByld('submit-btn2');
+function print(text) {
+  gameText.innerText +=text + "\n";
+  gameText.scrollTop =gameText.scrollHeight;
+}
