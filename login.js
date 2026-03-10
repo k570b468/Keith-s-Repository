25b1bc47b699a7137a760830520a3ea86e490563
k570b468 <55644723+k570b468@users.noreply.github.com>
2026-03-10 09:11:37 -0500

    Update login.js

--- login.js
+++ login.js
@@ -5,3 +5,8 @@ function print(text) {
   gameText.innerText +=text + "\n";
   gameText.scrollTop =gameText.scrollHeight;
 }
+function sleep(ms) {
+  return new Promise(resolve=> setTimeout(resolve, ms));
+}
+let currentStep="askUsername";
+let username="";
